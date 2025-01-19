b17663e333bc19ddc16eeb702cd618e7cc35f789
Siddharth11Roy <155230297+Siddharth11Roy@users.noreply.github.com>
2025-01-20 00:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("MY_API_KEY")
 
-os.environ["OPENAI_API_KEY"] = "api_key"
+os.environ["OPENAI_API_KEY"] = api_key
 
 
 client = OpenAI()
